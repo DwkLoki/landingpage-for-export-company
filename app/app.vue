6377767390a908482c40db0a5a54569c89fdc1e8
Dwiky Darmawansyah <dwiky.darmawansyah@gmail.com>
2026-06-14 11:40:18 +0800

    feat: image and SEO optimization

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,46 @@
 <script setup lang="ts">
+const { t } = useI18n();
+
 useHead({
-  titleTemplate: (title) => (title ? `${title} | PT. NIC` : "PT. NIC"),
+  titleTemplate: (title) =>
+    title
+      ? `${title} | PT. Nurhayati Indo Cemerlang`
+      : "PT. Nurhayati Indo Cemerlang",
+
+  script: [
+    {
+      type: "application/ld+json",
+      innerHTML: JSON.stringify({
+        "@context": "https://schema.org",
+        "@type": "Organization",
+
+        name: "PT. Nurhayati Indo Cemerlang",
+
+        url: "https://www.nurhayatiindocemerlang.com",
+
+        logo: "https://www.nurhayatiindocemerlang.com/main-logo.png",
+
+        description:
+          "Indonesian exporter of natural food products, spices, and processed food products.",
+
+        address: {
+          "@type": "PostalAddress",
+          addressLocality: "Makassar",
+          addressRegion: "South Sulawesi",
+          addressCountry: "ID",
+        },
+
+        areaServed: ["Southeast Asia", "Australia", "Europe"],
+      }),
+    },
+  ],
+});
+
+useSeoMeta({
+  description: () => t("meta.description"),
+  ogSiteName: "PT. Nurhayati Indo Cemerlang",
+  ogType: "website",
+  twitterCard: "summary_large_image",
 });
 </script>
 <template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,8 +5,35 @@ export default defineNuxtConfig({
   css: ["~/assets/css/tailwind.css"],
   app: {
     head: {
-      title: "PT. NIC",
-      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+      title: "PT. Nurhayati Indo Cemerlang",
+      link: [
+        {
+          rel: "icon",
+          type: "image/x-icon",
+          href: "/favicon.ico",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "32x32",
+          href: "/favicon-32x32.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "16x16",
+          href: "/favicon-16x16.png",
+        },
+        {
+          rel: "apple-touch-icon",
+          sizes: "180x180",
+          href: "/apple-touch-icon.png",
+        },
+        {
+          rel: "manifest",
+          href: "/site.webmanifest",
+        },
+      ],
     },
   },
   modules: [
