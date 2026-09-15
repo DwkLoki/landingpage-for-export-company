47e3c36815ec909efc2f00aeccb9d38717474d5c
Dwiky Darmawansyah <dwiky.darmawansyah@gmail.com>
2026-09-15 09:55:36 +0800

    style: update company title size

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -33,7 +33,9 @@ useHead({
       <div
         class="relative z-10 mx-auto max-w-4xl px-4 pt-24 pb-16 text-center sm:px-6"
       >
-        <h1 class="text-2xl font-bold text-blue-900 mb-4">
+        <h1
+          class="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-blue-900 mb-4"
+        >
           PT. Nurhayati Indo Cemerlang
         </h1>
         <h1
